391e957f718356bbd0157c87631ead1ab07a5416
jmilll <jwmiller.design@gmail.com>
2021-02-25 16:04:42 -0500

    Initial Commit

---
+++ src/components/CardList.js
@@ -0,0 +1,94 @@
+import React from 'react'
+
+const CardList = () => {
+
+
+    const images = [
+        {
+            id: 1,
+            src: '',
+            title: 'Ashiok, Nightmare Muse',
+            artist: 'Raymond Swanland',
+        },
+        {
+            id: 2,
+            src: '',
+            title: 'Chandra Ablaze',
+            artist: 'Steve Argyle',
+        },
+        {
+            id: 3,
+            src: '',
+            title: 'Garruk, Apex Predator',
+            artist: 'Tyler Jacobson',
+        },
+        {
+            id: 4,
+            src: '',
+            title: 'Jace, Wielder of Mysteries',
+            artist: 'Anna Steinbauer',
+        },
+        {
+            id: 5,
+            src: '',
+            title: 'Liliana, Death Mage',
+            artist: 'Kieran Yanner',
+        },
+        {
+            id: 6,
+            src: '',
+            title: 'Ajani, Valiant Protector',
+            artist: 'Anna Steinbauer',
+        },
+    ]
+
+    const imagesHard = [
+        {
+            id: 7,
+            src: '',
+            title: 'Ashiok, Nightmare Weaver',
+            artist: 'Karla Ortiz',
+        },
+        {
+            id: 8,
+            src: '',
+            title: 'Chandra, Torch of Defiance',
+            artist: 'Magali Villeneuve',
+        },
+        {
+            id: 9,
+            src: '',
+            title: 'Garruk, the Veil-Cursed',
+            artist: 'Grzegorz Rutkowski',
+        },
+        {
+            id: 10,
+            src: '',
+            title: 'Jace, the Living Guildpact ',
+            artist: 'Chase Stone',
+        },
+        {
+            id: 11,
+            src: '',
+            title: 'Liliana, the Necromancer',
+            artist: 'Livia Prima',
+        },
+        {
+            id: 12,
+            src: '',
+            title: 'Ajani, Mentor of Heroes',
+            artist: 'Aaron Miller',
+        },
+    ]
+
+
+
+
+    return (
+        <div>
+            Card
+        </div>
+    )
+}
+
+export default CardList
