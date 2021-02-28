2df983bb0ff49429ea829ab58064211652cbead3
jmilll <jwmiller.design@gmail.com>
2021-02-27 20:22:50 -0500

    implamenting difficutly useEffect

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {useState, useEffect} from 'react';
 //doesnt work unless have to import img instead of directly listing as src?
 import ajani1 from '../images/Ajani-Valiant-Protector.jpg'
 import ajani2 from '../images/Ajani-Mentor-of-Heroes.jpg'
@@ -37,7 +37,7 @@ import liliana2 from '../images/Liliana-the-Necromancer.jpg'
 
 const CardList = (props) => {
 
-    const {checkMemory} = props
+    const {score, checkMemory, difficulty} = props
 
     const images = [
         {
@@ -78,7 +78,7 @@ const CardList = (props) => {
         },
     ]
 
-    const imagesHard = [
+    const imagesMed = [
         {
             id: 7,
             src: ashiok2,
@@ -117,15 +117,109 @@ const CardList = (props) => {
         },
     ]
 
+    // const imagesHard = [
+    //     {
+    //         id: 13,
+    //         src: ashiok3,
+    //         title: '',
+    //         artist: '',
+    //     },
+    //     {
+    //         id: 14,
+    //         src: chandra3,
+    //         title: '',
+    //         artist: '',
+    //     },
+    //     {
+    //         id: 15,
+    //         src: garruk3,
+    //         title: '',
+    //         artist: '',
+    //     },
+    //     {
+    //         id: 16,
+    //         src: jace3,
+    //         title: '',
+    //         artist: '',
+    //     },
+    //     {
+    //         id: 17,
+    //         src: liliana3,
+    //         title: '',
+    //         artist: '',
+    //     },
+    //     {
+    //         id: 18,
+    //         src: ajani3,
+    //         title: '',
+    //         artist: '',
+    //     },
+    // ]
 
-    const testClick = (e, f) => {
-        console.log(e)
-        //console.log(f)
+    //initializes images as the cards under useeffect
+    const [cards, setCards] = useState([])
+
+    const ez = [...images]
+    const med = [...images, ...imagesMed]
+    //const hard = [...images, ...imagesMed, ...imagesHard]
+
+    // const testClick = (e, f) => {
+    //     console.log(e)
+    //     //console.log(f)
+    // }
+
+    function logez() {
+        const ez = [...images]
+        console.log(ez.length)
     }
+
+    function logmed() {
+        const med = [...images, ...imagesMed]
+        console.log(med.length)
+    }
+
+    const randomize = (arr) => {
+        for (let i = 0; i < arr.length; i++) {
+            let rIndex = Math.floor(Math.random() * arr.length - 1)
+            [arr[rIndex], arr[i]]
+
+        }
+        
+    }
+
+    //randomizes cards on each card click, referencing the score as the change catalyst
+    useEffect(() => {
+        console.log('shuffle')
+
+        return () => {
+            //cleanup
+        }
+    }, [score])
+
+    //changes cards only when diff changes
+    useEffect(() => {
+        if (difficulty === 1) {
+            setCards(ez)
+        } else if (difficulty === 2) {
+            setCards(med)
+        } else {
+            console.log('too hard')
+            //setCards(hard)
+        }
+
+        return () => {
+            //cleanup
+        }
+    }, [difficulty])
+
+
 
     return (
         <section className='card-container'>
-            {images.map(img => {
+            
+            {/* <button onClick={() => {logez()}}>ez arr</button>
+            <button onClick={() => {logmed()}}>med arr</button> */}
+            {cards.map(img => {
                 return (
                     <div className='card' key={img.title} id={img.title}  onClick={checkMemory.bind(this, img.title)}>
                         <h2>{img.title}</h2>
@@ -135,16 +229,56 @@ const CardList = (props) => {
                 )
             })}
 
-{/* 
-            <ul>
-                {tasks.map((task) => {
-                    return <li key={uniqid()}>{task}</li>
-                })}
-            </ul> */}
+            {/* {images.map(img => {
+                return (
+                    <div className='card' key={img.title} id={img.title}  onClick={checkMemory.bind(this, img.title)}>
+                        <h2>{img.title}</h2>
+                        <img src={img.src} alt={img.title} />
+                        <p className='artist'>Artist: {img.artist}</p>
+                    </div>
+                )
+            })} */}
+
         </section>
 
 
     )
+
+
+
+
+
+
+    // if (difficulty === 1) {
+    //     return (
+    //         <section className='card-container'>
+                
+    //             <button onClick={() => {logez()}}>ez arr</button>
+    //             <button onClick={() => {logmed()}}>med arr</button>
+    
+    //             {images.map(img => {
+    //                 return (
+    //                     <div className='card' key={img.title} id={img.title}  onClick={checkMemory.bind(this, img.title)}>
+    //                         <h2>{img.title}</h2>
+    //                         <img src={img.src} alt={img.title} />
+    //                         <p className='artist'>Artist: {img.artist}</p>
+    //                     </div>
+    //                 )
+    //             })}
+    
+    //         </section>
+    //     )
+    // } else if (difficulty === 2) { 
+    //     return (
+    //         <h2>MEDIUM DIFF</h2>
+    //     )
+    // } else {
+    //     return (
+    //         <h2>HARD DIFF</h2>
+    //     )
+    // }
+
+        
 }
 
 export default CardList
