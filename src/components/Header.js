3099bc3ffee8190973a0c664a8f25db209cd79e9
jmilll <jwmiller.design@gmail.com>
2021-03-01 16:45:28 -0500

    finishing style

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,14 +7,15 @@ const Header = (props) => {
     return (
         <section className='header'>
             <h1>Magic Memory Game </h1>
-            <p>Get points for choosing a card, but don't click on the same card more than once!</p>
             <div className='scoreboard'>
+                <p className='instructions'>Get points for choosing a card, but don't click on the same card more than once! Cards randomize afer each click</p>
+            
                 <div className='personal-score'>
                     <p>Current Streak: {score}</p>
                     <p>Last Streak: {lastScore}</p>
                     <p>High Score: {hiScore}</p>
                 </div>
-                <p>Perfect score for this difficulty: {perfectScore} </p>
+                <p className='perfect-score'>Perfect score for this difficulty: {perfectScore} </p>
             </div>
         </section>
     )
