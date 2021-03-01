d7a37b5c58d20bb38d7cbc1f4e06458e35123ad8
jmilll <jwmiller.design@gmail.com>
2021-03-01 17:25:05 -0500

    tidy up code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import CardList from './components/CardList';
 import Header from './components/Header';
@@ -36,28 +35,19 @@ const App = () => {
 
   const handleHiScore = () => {
     score > hiScore ? setHiScore(score) : resetRound()
-    //setHiScore(score)
   }
 
   const handlePerfectScore = () => {
     difficulty == 1 ? setPerfectScore(6) : 
     difficulty == 2 ? setPerfectScore(12) :
     setPerfectScore(18)
-    //setHiScore(score)
   }
 
   const addChosenCard = (card) => {
-    //const newArr = cardsArr.concat(card)
-    //const newArr = [...cardsArr, card]
-    //setCardsArr(newArr)
     setCardsArr(prevArr => [...prevArr, card])
-    //console.log(card)
-    //console.log(newArr)
   }
 
   const checkMemory = (card) => {
-    console.log(card)
-    //console.log({cardsArr})
     if(!cardsArr.includes(card)) {
       console.log('keep going')
       addChosenCard(card)
@@ -69,17 +59,6 @@ const App = () => {
       resetRound()
     }
   }
-
-  // const changeDifficultyU = () => {
-  //   console.log('difficulty = ' + difficulty)
-  //   setDifficulty((prevDifficulty) => prevDifficulty + 1)
-  // }
-
-  // const changeDifficultyD = () => {
-  //   console.log('difficulty = ' + difficulty)
-  //   setDifficulty((prevDifficulty) => prevDifficulty - 1)
-  // }
-
 
   const changeDifficulty = (e) => {
     let x = Number(e.target.value)
@@ -95,60 +74,14 @@ const App = () => {
     })
   }
 
-
-  // const changeDifficultyEasy = (e) => {
-  //   setDifficulty(1)
-  //   removeSelections()
-  //   e.target.classList.add('selected')
-  // }
-
-  // const changeDifficultyMedium = (e) => {
-  //   setDifficulty(2)
-  //   removeSelections()
-  //   e.target.classList.add('selected')
-  // }
-
-  // const changeDifficultyHard = (e) => {
-  //   setDifficulty(3)
-  //   removeSelections()
-  //   e.target.classList.add('selected')
-  // }
-
-  // const changeDifficultyExtreme = (e) => {
-  //   setDifficulty(4)
-  //   removeSelections()
-  //   e.target.classList.add('selected')
-  // }
-
   useEffect(() => {
     handlePerfectScore()
     resetAll()
   }, [difficulty])
-
   
-
-
-  // onClick={changeDifficultyMedium.bind(this)}
-
-  // onClick={checkMemory.bind(this, img.title)}
-  // <div className='difficulty-container'>
-  //  <button className='btn selected' onClick={() => changeDifficultyEasy()}>Easy</button>
-  //  <button className='btn' onClick={changeDifficultyMedium.bind(this)}>Medium</button>
-  //  <button className='btn' onClick={() => changeDifficultyHard()}>Hard</button>
-  //  <button className='btn extreme' onClick={() => changeDifficultyExtreme()}>Extreme</button>
-  // </div>
-
-//   <div className='difficulty-container'>
-//   <button className='btn selected' onClick={changeDifficultyEasy.bind(this)}>Easy</button>
-//   <button className='btn' onClick={changeDifficultyMedium.bind(this)}>Medium</button>
-//   <button className='btn' onClick={changeDifficultyHard.bind(this)}>Hard</button>
-//   <button className='btn extreme' onClick={changeDifficultyExtreme.bind(this)}>Extreme</button>
-// </div>
-
 
   return (
     <div className="App">
-      {/* <p>{cardsArr}</p> */}
       <Header score={score} lastScore={lastScore} hiScore={hiScore} perfectScore={perfectScore} />
      
       <div className='difficulty-container'>
